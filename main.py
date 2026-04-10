8afe8974ff056b56794691d6a8ccdcb54d77ad31
Денис Заставний <denys.zastavnyi@lpnu.ua>
2026-04-10 09:34:47 +0300

    Denys commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pyspark.sql import SparkSession
 from extractor import get_trip_data_schema, get_fare_data_schema, load_data
-
+from preprocessor import show_basic_statistics, preprocess_data
 
 def main():
     print("Ініціалізація Spark...")
@@ -27,8 +27,29 @@ def main():
     print("\n--- Перші 5 записів Fare Data ---")
     fare_df.show(5)
 
+
+    #show_basic_statistics(trip_df, "Trip Data")
+    show_basic_statistics(fare_df, "Fare Data")
+
+
+
+
+    print("Попередня обробка для зрізу 50тис.")
+    trip_df_test = trip_df.limit(50000)
+    fare_df_test = fare_df.limit(50000)
+
+    # Запускаємо очищення для тестових даних
+    trip_df_cleaned = preprocess_data(trip_df_test, "Trip Data")
+    fare_df_cleaned = preprocess_data(fare_df_test, "Fare Data")
+
+    # ПЕРЕВІРКА: показуємо статистику ПІСЛЯ очищення
+    # Тепер min у trip_distance має бути > 0, а max у часі — адекватним
+    print("\n--- СТАТИСТИКА ПІСЛЯ ОЧИЩЕННЯ ")
+    trip_df_cleaned.select("passenger_count", "trip_time_in_secs", "trip_distance").summary().show()
+
+    print("\n--- СТАТИСТИКА Fare Data ПІСЛЯ ОЧИЩЕННЯ ")
+    fare_df_cleaned.select("fare_amount", "tip_amount", "total_amount").summary().show()
+
     spark.stop()
-
-
 if __name__ == "__main__":
     main()
